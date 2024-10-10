ed2fe589266ace4479c6a2e1eb7ab0e61ed17d6d
MauroC0l <mauro.colopi01@gmail.com>
2024-10-10 10:09:08 +0200

    boh

--- progetto/src/main/java/entity/classi_astratte/VagoneRistorante.java
+++ progetto/src/main/java/entity/classi_astratte/VagoneRistorante.java
@@ -6,7 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "vagoneRistorante")
+@Table(name = "Vagoni Ristorante")
 public abstract class VagoneRistorante extends Vagone{ //pensare se si puo' levare abstract per il dao
 
 	@Column(name="coperti", nullable = false, unique = false)
