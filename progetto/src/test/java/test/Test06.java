303520988bc7d4e0e561172c613d8d1920b90def
RobertoGol23 <roberto.goldin99@gmail.com>
2024-10-11 10:00:50 +0200

    update test 06

--- progetto/src/test/java/test/Test06.java
+++ progetto/src/test/java/test/Test06.java
@@ -1,20 +1,67 @@
 package test;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+
+import configuration.JpaConfig;
+import eccezioni.eccezioniSigla.SiglaTrenoException;
+import entity.classi_astratte.FabbricaVagoni;
+import entity.classi_astratte.TrenoBuilder;
+import entity.dao.TrenoDAO;
+import entity.dao.UserDAO;
+import entity.treno.Treno;
+import entity.user.User;
+import fabbriche.FabbricaKargoModelz;
+import fabbriche.FabbricaRegionalGain;
+import utility.Assemblatore;
+
 public class Test06 {
     public static void main(String[] args) {
         
         /*				TEST 06
-         *  Test su qualcosa
+         *  Test creazione piu' treni
          *
          *
          *
          *
          */
+    	 
+        FabbricaVagoni fabbricaKM= new FabbricaKargoModelz();
+		TrenoBuilder builderKM = new Assemblatore(fabbricaKM);
+		
+		FabbricaVagoni fabbricaRG= new FabbricaRegionalGain();
+		TrenoBuilder builderRG = new Assemblatore(fabbricaRG);
+		
+		FabbricaVagoni fabbricaFF= new FabbricaKargoModelz();
+		TrenoBuilder builderFF = new Assemblatore(fabbricaFF);
+		
+        String sigla = "hprp";
+		try
+		{
+			AbstractApplicationContext context = new AnnotationConfigApplicationContext(JpaConfig.class);
+		
+			User mazza = new User("Salvatore","Mazza", "salvatore.mazza@gmail.com", "Danzacudur0_04");
+			UserDAO userDAO = context.getBean(UserDAO.class);
+			userDAO.salvaUser(mazza);
+			
+			Treno trenoKM = builderKM.costruisciTreno("Treno Cargo Passeggeri",sigla,mazza, 3);
+			Treno trenoRG = builderRG.costruisciTreno("Treno Cargo Passeggeri",sigla,mazza, 2);
+			Treno trenoFF = builderFF.costruisciTreno("Treno Cargo Passeggeri",sigla,mazza, 1);
 
-        
-        
-
-
-
-    }
+			TrenoDAO trenoDAO = context.getBean(TrenoDAO.class);
+            trenoDAO.salvaTreno(trenoKM);
+            trenoDAO.salvaTreno(trenoRG);
+            trenoDAO.salvaTreno(trenoFF);
+       
+			context.close();
+			
+		}
+		catch (SiglaTrenoException e)
+		{
+			String message = e.getMessage();
+			System.out.println(message);
+			System.out.println(e.getSuggerimento());
+		}
+				
+	}
 }
